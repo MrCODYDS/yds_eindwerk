4611c9a3b3fda2831c4dd77460d816287c750e81
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-04-28 16:23:56 +0200

    adding footer & small styling for footer & header

--- wp-content/themes/yds/footer.php
+++ wp-content/themes/yds/footer.php
@@ -1,3 +1,19 @@
-<?php wp_footer(); ?>
-</body>
-</html>
+<footer class="c-footer">
+    <div class="container">
+        <div class="footer-top row">
+            <div class="footer-top__title col d-flex justify-content-center align-items-center">
+                <p>Copyright 2020 - Lorem ipsum dolor sit amet consectetur</p>
+                <div>
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom row">
+            <div class="col">
+
+            </div>
+        </div>
+    </div>
+</footer>
