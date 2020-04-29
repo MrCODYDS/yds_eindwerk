366ba1819eca8ec707a40625a3baa510ffb8e1f8
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-04-29 16:43:11 +0200

    add hero block & hero scss & test-img

---
+++ wp-content/themes/yds/templates/blocks/block-hero.php
@@ -0,0 +1,36 @@
+<?php
+$spacer_top = (get_field('spacing_top_hero')) ? 'pt-5 pt-md-4 pt-lg-5' : '';
+$spacer_bottom = (get_field('spacing_bottom_hero')) ? 'pb-5 pb-md-4 pb-lg-5' : '';
+$text_align = strtolower('text-'.get_field('text_align_hero'));
+$image = get_field('image_hero');
+$title = get_field('title_hero');
+$text = get_field('text_hero');
+$cta = get_field('cta_button_hero');
+$cta_settings = get_field('cta_settings_hero');
+
+?>
+
+<section class="block b-hero <?= $text_align; ?>">
+    <div class="b-hero__img" style="background-image: url(<?= wp_get_attachment_image_url($image,  'full'); ?>)"></div>
+    <div class="b-hero__content py-4 py-md-0">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex flex-column align-items-center">
+                    <?php if($title): ?>
+                        <h1 class="b-hero__title mb-0"><?= $title; ?></h1>
+                    <?php endif; ?>
+
+                    <?php if($text): ?>
+                        <p><?= $text; ?></p>
+                    <?php endif; ?>
+
+                    <?php if($cta): ?>
+                        <a href="<?= $cta_settings["cta_link_hero"]["url"]; ?>" class="btn btn-<?= $cta_settings["cta_type_hero"]; ?>">
+                            <?= $cta_settings["cta_text_hero"]; ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
