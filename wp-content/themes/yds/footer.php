b141fc75869642742070605a02f66729c4b816a7
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-01 14:37:08 +0200

    added footer styling & added nav styling for footer & added dark blue color

--- wp-content/themes/yds/footer.php
+++ wp-content/themes/yds/footer.php
@@ -1,18 +1,29 @@
-<footer class="c-footer">
+<footer class="c-footer py-5 py-md-4 py-lg-5">
     <div class="container">
         <div class="footer-top row">
-            <div class="footer-top__title col d-flex justify-content-center align-items-center">
-                <p>Copyright 2020 - Lorem ipsum dolor sit amet consectetur</p>
-                <div>
-                    <img src="" alt="">
-                    <img src="" alt="">
-                    <img src="" alt="">
-                </div>
+            <div class="col-3 footer-top-content">
+                <img src="" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
+            </div>
+            <div class="col-3 footer-top-content">
+                <h5>Handige links</h5>
+                <nav role="navigation" class="c-nav-footer">
+                    <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'primary_navigation')); ?>
+                </nav>
+            </div>
+            <div class="col-3 footer-top-content">
+                <h5>Praktische zaken</h5>
+                <nav role="navigation" class="c-nav-footer">
+                    <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'footer_navigation')); ?>
+                </nav>
+            </div>
+            <div class="col-3 footer-top-content">
+                
             </div>
         </div>
         <div class="footer-bottom row">
             <div class="col">
-
+                <p>Copyright 2020 - Lorem ipsum dolor sit amet consectetur</p>
             </div>
         </div>
     </div>
