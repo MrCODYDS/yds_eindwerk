869c4fb10485e41797b9aab9547413e363c23b31
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-01 16:32:27 +0200

    adding responsiveness for footer

--- wp-content/themes/yds/footer.php
+++ wp-content/themes/yds/footer.php
@@ -1,8 +1,8 @@
 <footer class="c-footer">
-    <div class="c-footer-top py-2 py-md-4 py-lg-5">
+    <div class="c-footer-top py-3 py-md-4 py-lg-5">
         <div class="container">
             <div class="row">
-                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                <div class="c-footer-top__content col-lg-3 mb-4">
                     <div>
                         <div>
                             Dit is het logo
@@ -10,34 +10,33 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
                     </div>
                 </div>
-                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                <div class="c-footer-top__content col-sm-6 col-md-4 col-lg-3 mb-4">
                     <div>
                         <h5>Handige links</h5>
                         <nav role="navigation" class="c-nav-footer">
-                            <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'primary_navigation')); ?>
+                            <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => false, 'theme_location' => 'primary_navigation')); ?>
                         </nav>
                     </div>
                 </div>
-                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                <div class="c-footer-top__content col-sm-6 col-md-4 col-lg-3 mb-4">
                     <div>
                         <h5>Praktische zaken</h5>
                         <nav role="navigation" class="c-nav-footer">
-                            <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'footer_navigation')); ?>
+                            <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => false, 'theme_location' => 'footer_navigation')); ?>
                         </nav>
                     </div>
                 </div>
-                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                <div class="c-footer-top__content col-md-4 col-lg-3">
                     <div>
                         <h5>Contact</h5>
-                        <ul class="flex-column justify-content-center align-items-start">
-                            <li class="d-flex mb-3">
+                        <ul class="flex-column justify-content-center align-items-start text-center text-sm-left">
+                            <li class="d-flex mb-1">
                                 <img src="" alt="">
-                                <div>
-                                    <p>2000 Antwerpen (HQ)</p>
-                                    <p>Straatnaam 25- Belgium</p>
+                                <div class="">
+                                    <p><?php _e('Straatnaam 25</br>2000 Antwerpen', 'yds'); ?></p>
                                 </div>
                             </li>
-                            <li class="d-flex mb-3">
+                            <li class="d-flex mb-1">
                                 <img src="" alt="">
                                 <div>
                                     <p>yarne@info.com</p>
@@ -58,7 +57,7 @@
     <div class="c-footer-bottom py-4">
         <div class="container">
             <div class="row">
-                <div class="col d-flex justify-content-between">
+                <div class="col d-flex flex-column flex-md-row justify-content-between align-items-center text-center text-md-left">
                     <p>Copyright 2020 - Lorem ipsum dolor sit amet consectetur</p>
                     <nav role="navigation" class="c-nav-legal">
                         <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => false, 'theme_location' => 'legal_navigation')); ?>
