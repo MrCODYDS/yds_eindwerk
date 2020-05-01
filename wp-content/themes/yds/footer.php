9252b2766d5784932e9dfbc8f52c716d346478b5
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-01 15:26:52 +0200

    added footer styling & footer build up fix

--- wp-content/themes/yds/footer.php
+++ wp-content/themes/yds/footer.php
@@ -1,29 +1,66 @@
-<footer class="c-footer py-5 py-md-4 py-lg-5">
-    <div class="container">
-        <div class="footer-top row">
-            <div class="col-3 footer-top-content">
-                <img src="" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
-            </div>
-            <div class="col-3 footer-top-content">
-                <h5>Handige links</h5>
-                <nav role="navigation" class="c-nav-footer">
-                    <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'primary_navigation')); ?>
-                </nav>
-            </div>
-            <div class="col-3 footer-top-content">
-                <h5>Praktische zaken</h5>
-                <nav role="navigation" class="c-nav-footer">
-                    <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'footer_navigation')); ?>
-                </nav>
-            </div>
-            <div class="col-3 footer-top-content">
-                
+<footer class="c-footer">
+    <div class="c-footer-top py-5 py-md-4 py-lg-5">
+        <div class="container">
+            <div class="row">
+                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                    <div>
+                        <div>
+                            Dit is het logo
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
+                    </div>
+                </div>
+                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                    <div>
+                        <h5>Handige links</h5>
+                        <nav role="navigation" class="c-nav-footer">
+                            <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'primary_navigation')); ?>
+                        </nav>
+                    </div>
+                </div>
+                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                    <div>
+                        <h5>Praktische zaken</h5>
+                        <nav role="navigation" class="c-nav-footer">
+                            <?php wp_nav_menu(array('container' => 'ul', 'menu_class' => "flex-column align-items-start", 'theme_location' => 'footer_navigation')); ?>
+                        </nav>
+                    </div>
+                </div>
+                <div class="c-footer-top__content col-3 d-flex justify-content-center">
+                    <div>
+                        <h5>Contact</h5>
+                        <ul class="flex-column justify-content-center align-items-start">
+                            <li class="d-flex mb-3">
+                                <img src="" alt="">
+                                <div>
+                                    <p>2000 Antwerpen (HQ)</p>
+                                    <p>Straatnaam 25- Belgium</p>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-3">
+                                <img src="" alt="">
+                                <div>
+                                    <p>yarne@info.com</p>
+                                </div>
+                            </li>
+                            <li class="d-flex">
+                                <img src="" alt="">
+                                <div>
+                                    <p>+32 1 23 45 56 78</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="footer-bottom row">
-            <div class="col">
-                <p>Copyright 2020 - Lorem ipsum dolor sit amet consectetur</p>
+    </div>
+    <div class="c-footer-bottom py-5 py-md-4 py-lg-5">
+        <div class="container">
+            <div class="footer-bottom row">
+                <div class="col">
+                    <p>Copyright 2020 - Lorem ipsum dolor sit amet consectetur</p>
+                </div>
             </div>
         </div>
     </div>
