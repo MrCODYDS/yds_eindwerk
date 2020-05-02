5d645b9f4bd2a030e4616acfa432698f96f5d2dd
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-02 16:58:57 +0200

    added block-cta & styling & responsiveness

---
+++ wp-content/themes/yds/templates/blocks/block-cta.php
@@ -0,0 +1,35 @@
+<?php
+$spacer_top = (get_field('spacing_top_cta')) ? 'pt-5 pt-md-4 pt-lg-5' : '';
+$spacer_bottom = (get_field('spacing_bottom_cta')) ? 'pb-5 pb-md-4 pb-lg-5' : '';
+$text_align = 'text-'.get_field('text_align_cta');
+$title = get_field('title_cta');
+$text = get_field('text_cta');
+$cta = get_field('cta_button_cta');
+$cta_settings = get_field('cta_settings');
+
+?>
+
+<section class="block b-cta <?= $text_align; ?> <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="b-cta__content">
+                    <?php if($title): ?>
+                        <h2><?= $title; ?></h2>
+                    <?php endif; ?>
+
+                    <?php if($text): ?>
+                        <p class="mx-0 mx-md-8"><?= $text; ?></p>
+                    <?php endif; ?>
+                </div>
+
+                <?php if($cta): ?>
+                    <a href="<?= $cta_settings['cta_link_cta']['url']; ?>" class="btn btn-<?= $cta_settings['cta_type_cta']; ?> px-4">
+                        <?= $cta_settings['cta_text_cta']; ?>
+                    </a>
+                <?php endif; ?>
+            </div>
+        </div>
+
+    </div>
+</section>
