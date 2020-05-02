2ea9413ce5150024cc48dd5f19b2aa717a681f3d
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-02 10:22:02 +0200

    added styling for block-text-extra & changed heading-font-weight

--- wp-content/themes/yds/templates/blocks/block-text-extra.php
+++ wp-content/themes/yds/templates/blocks/block-text-extra.php
@@ -5,22 +5,31 @@ $image_align = get_field('image_align_textextra');
 $image = get_field('image_textextra');
 $title = get_field('title_textextra');
 $text = get_field('text_textextra');
+$cta = get_field('cta_button_textextra');
+$cta_settings = get_field('cta_settings_textextra');
 
 ?>
 
-<section class="block b-text-image <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
+<section class="block b-text-extra <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
     <div class="container">
         <div class="row">
-            <div class="b-text-image__image col-lg-5 <?php if($image_align == 'right'): ?>order-md-last offset-lg-1<?php endif; ?> d-flex align-items-center mb-3 mb-lg-0">
+            <div class="b-text-extra__image col-lg-5 <?php if($image_align == 'right'): ?>justify-content-end order-lg-last offset-lg-1<?php endif; ?> d-flex align-items-center mb-3 mb-lg-0  pb-4">
                 <?= wp_get_attachment_image($image, 'full', false, array("title" => get_the_title($image), 'class' => 'img-fluid')) ?>
+                <div class="b-text-extra__block d-none d-lg-block <?php if($image_align == 'right'): ?>b-text-extra__block--left <?php endif; ?> "></div>
             </div>
-            <div class="col-lg-6 <?php if($image_align == 'left'): ?>offset-lg-1<?php endif; ?> d-flex flex-column justify-content-center">
+            <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start <?php if($image_align == 'left'): ?>offset-lg-1<?php endif; ?>">
                 <?php if($title): ?>
                     <h2><?= $title; ?></h2>
                 <?php endif; ?>
 
                 <?php if($text): ?>
                     <p><?= $text; ?></p>
+                <?php endif; ?>
+
+                <?php if($cta): ?>
+                    <a href="<?= $cta_settings["cta_link_textextra"]["url"]; ?>" class="btn btn-<?= $cta_settings["cta_type_textextra"]; ?> mt-2">
+                        <?= $cta_settings["cta_text_textextra"]; ?>
+                    </a>
                 <?php endif; ?>
             </div>
         </div>
