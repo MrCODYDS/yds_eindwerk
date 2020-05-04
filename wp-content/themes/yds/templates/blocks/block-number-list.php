029977b762eba9ff1080a24c5e9774577d9a8390
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-04 14:47:43 +0200

    changed loop for checking number amount block-number-list

--- wp-content/themes/yds/templates/blocks/block-number-list.php
+++ wp-content/themes/yds/templates/blocks/block-number-list.php
@@ -2,6 +2,7 @@
 $spacer_top = (get_field('spacing_top_numberlist')) ? 'pt-5 pt-md-4 pt-lg-5' : '';
 $spacer_bottom = (get_field('spacing_bottom_numberlist')) ? 'pb-5 pb-md-4 pb-lg-5' : '';
 $title = get_field('title_numberlist');
+$repeater = get_field('repeater_numberlist');
 
 ?>
 
@@ -16,25 +17,26 @@ $title = get_field('title_numberlist');
         </div>
         <div class="row justify-content-between mx-lg-4 mx-xl-6 mx-xxl-8">
             
-            <?php if(get_field('repeater_numberlist')): $i = 0; ?>
-                <?php while(has_sub_field('repeater_numberlist')): $i++; ?>
+            <?php if($repeater): $i = 0; ?>
+                <?php foreach($repeater as $column): $i++ ?>
                     <div class="col-lg-6 d-flex align-items-center mb-4">
                         <div class="b-numberlist__number col-2">
-                            <h2><?php echo "0" .$i ?></h2>
+                            <h2><?php echo "0" . $i ?></h2>
                         </div>
                         <div class="col-10">
-                            <?php if(the_sub_field('repeater_title_numberlist')): ?>
-                                <h3><?= the_sub_field('repeater_title_numberlist'); ?></h3>
+                            <?php if($column['repeater_title_numberlist']): ?>
+                                <h3><?= $column['repeater_title_numberlist']; ?></h3>
                             <?php endif; ?>
-
-                            <?php if(the_sub_field('repeater_text_numberlist')): ?>
-                                <p class="m-lg-0"><?= the_sub_field('repeater_text_numberlist'); ?></p>
+                            <?php if($column['repeater_text_numberlist']): ?>
+                                <p><?= $column['repeater_text_numberlist']; ?></p>
                             <?php endif; ?>
                         </div>
                     </div>
-                <?php endwhile; ?>
+                    <?php endforeach; ?>
             <?php endif; ?>
             </div>
         </div>
     </div>
 </div>
+            
+            
