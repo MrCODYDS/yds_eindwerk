67bbea0231a86039abd9619ce75b927c20ebd95e
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-10 15:35:31 +0200

    added modals & functions for next

--- wp-content/themes/yds/src/js/main.js
+++ wp-content/themes/yds/src/js/main.js
@@ -14,3 +14,80 @@ document.querySelector('#reservation-form')
 
 document.querySelector('.modal__header span')
   .addEventListener('click', toggleModal);
+
+
+
+/* 
+ * Via quertselector btn-next ophalen
+ * Onlick data-next of data-prvious valie ophalen
+ * Alle form-part 's hiden
+ * Tonen van opgehaalde class
+*/
+
+function showTest(button) {
+  const data = button.dataset.next;
+
+  const formParts = document.querySelectorAll('.form-part');
+  formParts.forEach(function(part, index) {
+    part.classList.add('form-part--hidden');
+  });
+
+  document.querySelector('.' + data).classList.remove('form-part--hidden');
+}
+
+const buttonsNext = document.querySelectorAll('.btn-next');
+buttonsNext.forEach(function(button, index) {
+    button.addEventListener('click', function(){
+      showTest(this);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+const previous = () => {
+  // Get btn-next data field value
+  const dataValue = document.querySelector('.btn-previous');
+
+  // Hide all form-parts
+  if(!document.querySelector('.form-choices').classList.contains('form-part--hidden')) {
+    document.querySelector('.form-choices')
+    .classList.add('form-part--hidden');
+  }
+
+  if(!document.querySelector('.form-dates').classList.contains('form-part--hidden')) {
+    document.querySelector('.form-dates')
+    .classList.add('form-part--hidden');
+  }
+
+  if(!document.querySelector('.form-times').classList.contains('form-part--hidden')) {
+    document.querySelector('.form-times')
+    .classList.add('form-part--hidden');
+  }
+
+  if(!document.querySelector('.form-people').classList.contains('form-part--hidden')) {
+    document.querySelector('.form-people')
+    .classList.add('form-part--hidden');
+  }
+  
+
+
+  document.querySelector('.' + dataValue.dataset.previous)
+    .classList.toggle('form-part--hidden');
+}
+
+document.querySelector('.btn-previous')
+  .addEventListener('click', previous);
--- wp-content/themes/yds/templates/includes/reservation.php
+++ wp-content/themes/yds/templates/includes/reservation.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 ?>
 
 <section>
@@ -6,6 +9,9 @@
         <button id="show-modal">Modal</button>
     </div>
 </section>
+
+
+
 <div class="modal modal--hidden">
     <div class="modal__dialog">
         <div class="modal__header">
@@ -13,9 +19,10 @@
         </div>
         <div class="modal__content">
             
-            <form action="" class="reservation-form" id="reservation-form">
-                <!--<div class="form-choices">
-                    <h3 class="mb-5">Maak je keuze</h3>
+            <form action="" class="reservation-form" id="reservation-form" method="post">
+                <h3 class="mb-5">Reservatie</h3>
+                <div class="form-choices form-part">
+                    <p class="mb-3">Maak je keuze</p>
                     <div class="row form-choices__content">
                         <div class="form-choices__button col mr-5 mb-5">
                             <svg class="icon"><use xlink:href="#badminton" /></svg>
@@ -36,18 +43,99 @@
                         </div>
                     </div>
                     <div class="w-100 text-right mt-5">
-                        <button class="btn btn-link btn-link--primary">Volgende</button>
-                        <svg class="icon icon--arrow"><use xlink:href="#arrow" /></svg>
-                    </div>
-                </div>-->
-                <div class="form-dates">
-                    <h3 class="mb-5">Maak je keuze</h3>
-                    <div id="datepicker"></div>
-                    <div class="w-100 text-right mt-5">
-                        <button class="btn btn-link btn-link--primary">Volgende</button>
-                        <svg class="icon icon--arrow"><use xlink:href="#arrow" /></svg>
+                        <button type="button" data-next="form-dates" class="btn btn-link btn-link--primary btn-next">Volgende</button>
+                        <svg class="icon icon--arrow"><use xlink:href="#arrow-right" /></svg>
                     </div>
                 </div>
+                <div class="form-dates form-part form-part--hidden">
+                    <p class="mb-3">Kies een datum</p>
+                    <div id="datepicker"></div>
+                    <div class="d-flex justify-content-between w-100 mt-5">
+                        <div>
+                            <svg class="icon icon--arrow"><use xlink:href="#arrow-left" /></svg>
+                            <button type="button" data-previous="form-choices" class="btn btn-link btn-link--primary btn-previous">Vorige</button>
+                        </div>
+                        <div>
+                            <button type="button" data-next="form-times" class="btn btn-link btn-link--primary btn-next">Volgende</button>
+                            <svg class="icon icon--arrow"><use xlink:href="#arrow-right" /></svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-times form-part form-part--hidden">
+                    <div class="form-times__sessions row flex-column mb-5">
+                        <div class="col">
+                        <p class="mb-3">Kies de lengte van de sessie</p>
+                            <select name="sessions" id="sessions">
+                                <option value="" selected="selected" disabled hidden>Geen tijd gekozen</option>
+                                <option value="1">1 uur</option>
+                                <option value="2">2 uur</option>
+                                <option value="3">3 uur</option>
+                                <option value="4">4 uur</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-times__timeslots row flex-column">
+                        <div class="col">
+                            <p class="mb-3">Kies het tijdslot</p>
+                            <div class="row">
+                                <div class="timeslots-box col-4 mb-3">
+                                    <p class="m-0">08:00 - 10:00</p>
+                                </div>
+                                <div class="timeslots-box col-4 mb-3">
+                                    <p class="m-0">10:00 - 12:00</p>
+                                </div>
+                                <div class="timeslots-box col-4 mb-3">
+                                    <p class="m-0">12:00 - 14:00</p>
+                                </div>
+                                <div class="timeslots-box col-4 mb-3">
+                                    <p class="m-0">12:00 - 14:00</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between w-100 mt-5">
+                        <div>
+                            <svg class="icon icon--arrow"><use xlink:href="#arrow-left" /></svg>
+                            <button type="button" data-previous="form-dates" class="btn btn-link btn-link--primary btn-previous">Vorige</button>
+                        </div>
+                        <div>
+                            <button type="button" data-next="form-people" class="btn btn-link btn-link--primary btn-next">Volgende</button>
+                            <svg class="icon icon--arrow"><use xlink:href="#arrow-right" /></svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-people form-part form-part--hidden">
+                    <p class="mb-3">Kies het aantal personen</p>
+                    <div class="form-people row">
+                        <div class="col">
+                            <div class="row">
+                                <div class="people-box col-3 mb-3">
+                                    <p class="m-0">1</p>
+                                </div>
+                                <div class="people-box col-3 mb-3">
+                                    <p class="m-0">2</p>
+                                </div>
+                                <div class="people-box col-3 mb-3">
+                                    <p class="m-0">3</p>
+                                </div>
+                                <div class="people-box col-3 mb-3">
+                                    <p class="m-0">4</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between w-100 mt-5">
+                        <div>
+                            <svg class="icon icon--arrow"><use xlink:href="#arrow-left" /></svg>
+                            <button type="button" data-next="form-times" class="btn btn-link btn-link--primary btn-previous">Vorige</button>
+                        </div>
+                        <div>
+                            <button type="button" data-next="form-choices" class="btn btn-link btn-link--primary btn-next">Volgende</button>
+                            <svg class="icon icon--arrow"><use xlink:href="#arrow-right" /></svg>
+                        </div>
+                    </div>
+                </div>
+                <!--<button type="submit">Reserveren</button>-->
             </form>
         </div>
     </div>
