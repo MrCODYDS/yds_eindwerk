6302e2272b8be0765fa377dcd98d502ea493b8f6
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-12 19:57:42 +0200

    added registerpage & styling

--- wp-content/themes/yds/templates/template-register.php
+++ wp-content/themes/yds/templates/template-register.php
@@ -1,5 +1,5 @@
 <?php
-/* Template Name: Template - Login */
+/* Template Name: Template - Register */
 
 $redirect_to = '';
 ?>
@@ -8,35 +8,31 @@ $redirect_to = '';
 
 <section class="c-login d-flex align-items-center py-6">
     <div class="container">
-        <div class="c-login-content row justify-content-center">
+        <div class="c-login-content row justify-content-center ">
             <div class="c-login-content__login col-md-7 col-xl-5">
                 <div class='login-part-login d-flex justify-content-center align-items-center bg-white'>
-                    <form name="loginform" id="loginform" class="text-center" action="<?php echo site_url( '/wp-login.php' ); ?>" method="post">
+                    <form name="loginform" id="loginform" class="text-center" action="<?php echo site_url( '/wp-login.php?action=register' ); ?>" method="post">
                         <p>
                             <label for="user_login"></label>
                             <input id="user_login" type="text" value="" name="log" placeholder="Gebruikersnaam">
                         </p>
                         <p>
-                            <label for="user_pass"></label>
-                            <input id="user_pass" type="password" value="" name="pwd" placeholder="Wachtwoord">
-                        </p>
-                        <p>
-                            <input id="rememberme" type="checkbox" value="forever" name="rememberme">
-                            <label for="rememberme">Remember me</label>
+                            <label for="user_email"></label>
+                            <input id="user_email" type="email" value="" name="email" placeholder="Email">
                         </p>
                         <p class="mt-6 mb-0">
-                            <input id="wp-submit" class="btn btn-secondary" type="submit" value="Login" name="wp-submit">
+                            <input id="wp-submit" class="btn btn-secondary" type="submit" value="Registreer" name="wp-submit">
                             <input type="hidden" value="<?php echo esc_attr( $redirect_to ); ?>" name="redirect_to">
                             <input type="hidden" value="1" name="testcookie">
                         </p>
                     </form>
                 </div>
             </div>
-            <div class="c-login-content__register col-md-5 col-xl-3">
+            <div class="c-login-content__register col-md-5 col-xl-3 order-md-first">
                 <div class="login-part-register d-flex flex-column justify-content-center align-items-center text-center">
-                    <h5 class="text-white mb-4">Nog geen account?</h5>
+                    <h5 class="text-white mb-4">Al een account?</h5>
                     <div class="register-button py-3">
-                        <a href="" class="btn btn-outline btn-outline--blue">Registreer</a>
+                        <a href="" class="btn btn-outline btn-outline--blue">Log in</a>
                     </div>
                 </div>
             </div>
