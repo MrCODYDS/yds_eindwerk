a566938820237b0144e95b99ba6715d82e012341
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-13 16:23:24 +0200

    added changes to block-reservation & block-team

--- wp-content/themes/yds/templates/blocks/block-team.php
+++ wp-content/themes/yds/templates/blocks/block-team.php
@@ -4,17 +4,17 @@ $spacer_bottom = (get_field('spacing_bottom_team')) ? 'pb-5 pb-md-4 pb-lg-5' : '
 $text_align = strtolower('text-'.get_field('text_align_team'));
 $title = get_field('title_team');
 $text = get_field('text_team');
+$background = get_field('background_team');
 $repeater = get_field('repeater_team');
 $amount = get_field('number_of_columns_team');
 
 ?>
-
-<div class="block b-team <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
+<div class="block b-team <?php if($background): ?>b-team--lightblue<?php endif; ?> <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
     <div class="container">
         <div class="row flex-column <?= $text_align; ?>">
             <div class="col">
                 <?php if($title): ?>
-                    <h2 class="mb-3"><?= $title; ?></h2>
+                    <h2 class="mb-5"><?= $title; ?></h2>
                 <?php endif; ?>
 
                 <?php if($text): ?>
@@ -31,7 +31,7 @@ $amount = get_field('number_of_columns_team');
                     <?= wp_get_attachment_image($column['repeater_image_team'], 'full', false, array("title" => get_the_title($column['repeater_image_team']), 'class' => 'img-fluid mb-3')); ?>
 
                     <?php if($column['repeater_title_team']): ?>
-                        <h3><?= $column['repeater_title_team']; ?></h3>
+                        <h3 class="mb-3"><?= $column['repeater_title_team']; ?></h3>
                     <?php endif; ?>
 
                     <?php if($column['repeater_text_team']): ?>
