55b1cb76ab24ed9438e6233f4431c2a8664d4b69
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-26 17:49:11 +0200

    added block flex styling & changed html for block text image

--- wp-content/themes/yds/templates/blocks/block-flex.php
+++ wp-content/themes/yds/templates/blocks/block-flex.php
@@ -12,18 +12,19 @@ $amount = get_field('number_of_columns_flex');
     <div class="container">
         <div class="row">
             <?php foreach($repeater as $column): ?>
-                <?php if($amount == '2'): ?><div class="col-sm-12 col-md-6"><?php endif; ?>
-                <?php if($amount == '3'): ?><div class="col-sm-12 col-md-4"><?php endif; ?>
+                <?php if($amount == '2'): ?><div class="col-sm-12 col-md-6 mb-4 mb-lg-0"><?php endif; ?>
+                <?php if($amount == '3'): ?><div class="col-sm-12 col-lg-4 mb-4 mb-lg-0"><?php endif; ?>
+                    <div class="block-background h-100 py-5 px-4">
+                        <?= wp_get_attachment_image($column['image_flex'], 'full', false, array("title" => get_the_title($column['image_flex']), 'class' => 'img-fluid mb-3')); ?>
 
-                    <?= wp_get_attachment_image($column['image_flex'], 'full', false, array("title" => get_the_title($column['image_flex']), 'class' => 'img-fluid mb-3')); ?>
+                        <?php if($column['title_flex']): ?>
+                            <h3 class="mb-3"><?= $column['title_flex']; ?></h3>
+                        <?php endif; ?>
 
-                    <?php if($column['title_flex']): ?>
-                        <h3 class="mb-3"><?= $column['title_flex']; ?></h3>
-                    <?php endif; ?>
-
-                    <?php if($column['text_flex']): ?>
-                        <p class="m-0"><?= $column['text_flex']; ?></p>
-                    <?php endif; ?>
+                        <?php if($column['text_flex']): ?>
+                            <p class="m-0"><?= $column['text_flex']; ?></p>
+                        <?php endif; ?>
+                    </div>
                 </div>
             <?php endforeach; ?>
         </div>
--- wp-content/themes/yds/templates/blocks/block-text-image.php
+++ wp-content/themes/yds/templates/blocks/block-text-image.php
@@ -12,24 +12,28 @@ $cta_settings = get_field('cta_settings_textimage');
 
 <section class="block b-text-image <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
     <div class="container">
-        <div class="block-background row py-6 px-0 px-md-5">
-            <div class="b-text-image__image col-lg-5 <?php if($image_align == 'right'): ?>order-lg-last offset-lg-1<?php endif; ?> d-flex align-items-center mb-3 mb-lg-0">
-                <?= wp_get_attachment_image($image, 'full', false, array("title" => get_the_title($image), 'class' => 'img-fluid')) ?>
-            </div>
-            <div class="col-lg-6 <?php if($image_align == 'left'): ?>offset-lg-1<?php endif; ?> d-flex flex-column justify-content-center align-items-start">
-                <?php if($title): ?>
-                    <h2 class="m-0"><?= $title; ?></h2>
-                <?php endif; ?>
+        <div class="row">
+            <div class="col">
+                <div class="block-background d-flex flex-wrap py-6 px-0 px-md-5">
+                    <div class="b-text-image__image col-lg-5 <?php if($image_align == 'right'): ?>order-lg-last offset-lg-1<?php endif; ?> d-flex align-items-center mb-3 mb-lg-0">
+                        <?= wp_get_attachment_image($image, 'full', false, array("title" => get_the_title($image), 'class' => 'img-fluid')) ?>
+                    </div>
+                    <div class="col-lg-6 <?php if($image_align == 'left'): ?>offset-lg-1<?php endif; ?> d-flex flex-column justify-content-center align-items-start">
+                        <?php if($title): ?>
+                            <h2 class="m-0"><?= $title; ?></h2>
+                        <?php endif; ?>
 
-                <?php if($text): ?>
-                    <p class="my-3"><?= $text; ?></p>
-                <?php endif; ?>
+                        <?php if($text): ?>
+                            <p class="my-3"><?= $text; ?></p>
+                        <?php endif; ?>
 
-                <?php if($cta): ?>
-                    <a href="<?= $cta_settings["cta_link_textimage"]["url"]; ?>" class="btn btn-<?= $cta_settings["cta_type_textimage"]; ?>">
-                        <?= $cta_settings["cta_link_textimage"]['title']; ?>
-                    </a>
-                <?php endif; ?>
+                        <?php if($cta): ?>
+                            <a href="<?= $cta_settings["cta_link_textimage"]["url"]; ?>" class="btn btn-<?= $cta_settings["cta_type_textimage"]; ?>">
+                                <?= $cta_settings["cta_link_textimage"]['title']; ?>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
