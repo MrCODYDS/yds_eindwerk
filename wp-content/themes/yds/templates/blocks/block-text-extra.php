13a1da9f8d9e5bb42c7e8d9ff3676d7b6df63e7e
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-26 20:24:46 +0200

    added styling for block-text-extra

--- wp-content/themes/yds/templates/blocks/block-text-extra.php
+++ wp-content/themes/yds/templates/blocks/block-text-extra.php
@@ -13,24 +13,28 @@ $cta_settings = get_field('cta_settings_textextra');
 <section class="block b-text-extra <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
     <div class="container">
         <div class="row">
-            <div class="b-text-extra__image col-lg-5 <?php if($image_align == 'right'): ?>justify-content-end order-lg-last offset-lg-1<?php endif; ?> d-flex align-items-center mb-3 mb-lg-0  pb-4">
-                <?= wp_get_attachment_image($image, 'full', false, array("title" => get_the_title($image), 'class' => 'img-fluid')) ?>
-                <div class="b-text-extra__block d-none d-lg-block <?php if($image_align == 'right'): ?>b-text-extra__block--left <?php endif; ?> "></div>
-            </div>
-            <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start <?php if($image_align == 'left'): ?>offset-lg-1<?php endif; ?>">
-                <?php if($title): ?>
-                    <h2 class="m-0"><?= $title; ?></h2>
-                <?php endif; ?>
+            <div class="col">
+                <div class="block-background d-flex flex-wrap py-6 px-0 px-md-5">
+                    <div class="b-text-extra__image col-lg-5 <?php if($image_align == 'right'): ?>justify-content-end order-lg-last offset-lg-1<?php endif; ?> d-flex align-items-center mb-3 mb-lg-0  pb-4">
+                        <?= wp_get_attachment_image($image, 'full', false, array("title" => get_the_title($image), 'class' => 'img-fluid')) ?>
+                        <div class="b-text-extra__block d-none d-lg-block <?php if($image_align == 'right'): ?>b-text-extra__block--left <?php endif; ?> "></div>
+                    </div>
+                    <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start <?php if($image_align == 'left'): ?>offset-lg-1<?php endif; ?>">
+                        <?php if($title): ?>
+                            <h2 class="m-0"><?= $title; ?></h2>
+                        <?php endif; ?>
 
-                <?php if($text): ?>
-                    <p class="my-3"><?= $text; ?></p>
-                <?php endif; ?>
+                        <?php if($text): ?>
+                            <p class="my-3"><?= $text; ?></p>
+                        <?php endif; ?>
 
-                <?php if($cta): ?>
-                    <a href="<?= $cta_settings["cta_link_textextra"]["url"]; ?>" class="btn btn-<?= $cta_settings["cta_type_textextra"]; ?>">
-                        <?= $cta_settings["cta_link_textextra"]['title']; ?>
-                    </a>
-                <?php endif; ?>
+                        <?php if($cta): ?>
+                            <a href="<?= $cta_settings["cta_link_textextra"]["url"]; ?>" class="btn btn-<?= $cta_settings["cta_type_textextra"]; ?>">
+                                <?= $cta_settings["cta_link_textextra"]['title']; ?>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
