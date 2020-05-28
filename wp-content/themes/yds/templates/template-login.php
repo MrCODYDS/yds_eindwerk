59f4b6cd02212216c17715d73f2a7b8a77e57398
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-28 10:39:57 +0200

    added styling and html elements to login/register templates & added user-icon.svg

--- wp-content/themes/yds/templates/template-login.php
+++ wp-content/themes/yds/templates/template-login.php
@@ -4,35 +4,44 @@
 $redirect_to = '';
 ?>
 
-<section class="c-login py-6">
+<section class="c-login py-8">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-8">
+            <div class="col-9">
                 <div class="">
                     <div class="row c-login-content">
-                        <div class="c-login--login col-7 d-flex justify-content-center align-items-center">
-                            <form name="loginform" id="loginform" class="text-center" action="<?php echo site_url( '/wp-login.php' ); ?>" method="post">
-                                <p>
-                                    <label for="user_login"></label>
-                                    <input id="user_login" type="text" value="" name="log" placeholder="Gebruikersnaam">
-                                </p>
-                                <p>
-                                    <label for="user_pass"></label>
-                                    <input id="user_pass" type="password" value="" name="pwd" placeholder="Wachtwoord">
-                                </p>
-                                <p>
-                                    <input id="rememberme" type="checkbox" value="forever" name="rememberme">
-                                    <label for="rememberme">Remember me</label>
-                                </p>
-                                <p class="mt-3 mb-0">
-                                    <input id="wp-submit" class="btn btn-secondary" type="submit" value="Login" name="wp-submit">
-                                    <input type="hidden" value="<?php echo esc_attr( $redirect_to ); ?>" name="redirect_to">
-                                    <input type="hidden" value="1" name="testcookie">
-                                </p>
-                            </form>
+                        <div class="c-login--login col-7 d-flex flex-column justify-content-around">
+                            <div class="d-flex flex-column justify-content-center align-items-center h-100">
+                                <div class="c-login__image mb-5"></div>
+                                <form name="loginform" id="loginform" class="text-center" action="<?php echo site_url( '/wp-login.php' ); ?>" method="post">
+                                    <p>
+                                        <label for="user_login"></label>
+                                        <input id="user_login" type="text" value="" name="log" placeholder="Gebruikersnaam">
+                                    </p>
+                                    <p>
+                                        <label for="user_pass"></label>
+                                        <input id="user_pass" type="password" value="" name="pwd" placeholder="Wachtwoord">
+                                    </p>
+                                    <p>
+                                        <input id="rememberme" type="checkbox" value="forever" name="rememberme">
+                                        <label for="rememberme">Remember me</label>
+                                    </p>
+                                    <p class="mt-3 mb-0">
+                                        <input id="wp-submit" class="btn btn-secondary" type="submit" value="Login" name="wp-submit">
+                                        <input type="hidden" value="<?php echo esc_attr( $redirect_to ); ?>" name="redirect_to">
+                                        <input type="hidden" value="1" name="testcookie">
+                                    </p>
+                                </form>
+                            </div>
+                            <div class="text-center pb-3">
+                                <a href="/register" class="btn btn-link btn-link--dark">
+                                    Maak nu een account
+                                    <svg class="icon icon--arrow"><use xlink:href="#arrow-right" /></svg>
+                                </a>
+                            </div>
                         </div>
-                        <div class="c-login--register col-5 d-flex flex-column justify-content-center text-center">
-                            <div class="c-login__part--register px-3">
+                        <div class="c-login--info col-5 d-flex flex-column justify-content-center text-center">
+                            <div class="px-5">
                                 <h5 class="text-white mb-4">Welkom terug!</h5>
                                 <div class="text-white">
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor.</p>
--- wp-content/themes/yds/templates/template-register.php
+++ wp-content/themes/yds/templates/template-register.php
@@ -4,36 +4,45 @@
 $redirect_to = '';
 ?>
 
-<section class="c-login py-6">
+<section class="c-login py-8">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-8">
+            <div class="col-9">
                 <div class="">
                     <div class="row c-login-content">
-                        <div class="c-login--register col-5 d-flex flex-column justify-content-center text-center">
-                            <div class="c-login__part--register px-3">
-                                <h5 class="text-white mb-4">Welkom terug!</h5>
+                        <div class="c-register--info col-5 d-flex flex-column justify-content-center text-center">
+                            <div class="px-5">
+                                <h5 class="text-white mb-4">Welkom!</h5>
                                 <div class="text-white">
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="c-login--login col-7 d-flex justify-content-center align-items-center">
-                            <form name="loginform" id="loginform" class="text-center" action="<?php echo site_url( '/wp-login.php?action=register' ); ?>" method="post">
-                                <p>
-                                    <label for="user_login"></label>
-                                    <input id="user_login" type="text" value="" name="log" placeholder="Gebruikersnaam">
-                                </p>
-                                <p>
-                                    <label for="user_email"></label>
-                                    <input id="user_email" type="email" value="" name="email" placeholder="Email">
-                                </p>
-                                <p class="mt-6 mb-0">
-                                    <input id="wp-submit" class="btn btn-secondary" type="submit" value="Registreer" name="wp-submit">
-                                    <input type="hidden" value="<?php echo esc_attr( $redirect_to ); ?>" name="redirect_to">
-                                    <input type="hidden" value="1" name="testcookie">
-                                </p>
-                            </form>
+                        <div class="c-login--register col-7 d-flex flex-column justify-content-around">
+                            <div class=" d-flex flex-column justify-content-center align-items-center h-100">
+                                <div class="c-login__image mb-5"></div>
+                                <form name="loginform" id="loginform" class="text-center" action="<?php echo site_url( '/wp-login.php?action=register' ); ?>" method="post">
+                                    <p>
+                                        <label for="user_login"></label>
+                                        <input id="user_login" type="text" value="" name="log" placeholder="Gebruikersnaam">
+                                    </p>
+                                    <p>
+                                        <label for="user_email"></label>
+                                        <input id="user_email" type="email" value="" name="email" placeholder="Email">
+                                    </p>
+                                    <p class="mt-6 mb-0">
+                                        <input id="wp-submit" class="btn btn-secondary" type="submit" value="Registreer" name="wp-submit">
+                                        <input type="hidden" value="<?php echo esc_attr( $redirect_to ); ?>" name="redirect_to">
+                                        <input type="hidden" value="1" name="testcookie">
+                                    </p>
+                                </form>
+                            </div>
+                            <div class="text-center pb-3">
+                                <a href="/register" class="btn btn-link btn-link--dark">
+                                    Ik heb al een account
+                                    <svg class="icon icon--arrow"><use xlink:href="#arrow-right" /></svg>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
