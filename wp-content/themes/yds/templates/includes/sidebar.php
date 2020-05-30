0b6c758cc9deaaf0d87013809935b8d0cb7607f1
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-05-30 17:30:47 +0200

    added buttons to sidebar for login/logout & reservations/register

--- wp-content/themes/yds/templates/includes/sidebar.php
+++ wp-content/themes/yds/templates/includes/sidebar.php
@@ -8,17 +8,23 @@
             </div>
         </div>
         <div class="mt-5 px-4 pb-2">
-            <div class="text-center mb-4">
-                <h3 class="text-white">John Doe</h3>
-            </div>
-            <div class="d-flex justify-content-between">
-                <a href="" class="btn btn-outline btn-outline--mobile">Reservations</a>
-                <a href="" class="btn btn-outline btn-outline--mobile">Log out</a>
-            </div>
+            <?php if(is_user_logged_in()): ?>
+                <div class="text-center mb-4">
+                    <h3 class="text-white"><?php echo $current_user->user_login ?></h3>
+                </div>
+                <div class="d-flex justify-content-between">
+                <a href="/user-reservations" class="btn btn-outline btn-outline--mobile">Reservations</a>
+                    <a href="<?php echo wp_logout_url() ?>" class="btn btn-outline btn-outline--mobile">Log out</a>
+                </div>
+            <?php else: ?>
+                <div class="d-flex justify-content-between">
+                    <a href="/login" class="btn btn-outline btn-outline--mobile">Log In</a>
+                    <a href="/register" class="btn btn-outline btn-outline--mobile">Sign Up</a>
+                </div>
+            <?php endif; ?>
         </div>
         <hr class="c-sidebar__spacing mx-4">
         <nav role="navigation" class="c-nav-sidebar">
-            
             <?php
                 wp_nav_menu( array('theme_location'  => 'primary_navigation','container'  => false,'container_id'  => '','container_class' => '','menu_class'  => 'top-nav nav-right','echo'  => true,'items_wrap'  => '<ul id="%1$s" class="%2$s">%3$s</ul>','depth'  => 10, 'walker' => new fluent_themes_custom_walker_nav_menu) );
             ?>
