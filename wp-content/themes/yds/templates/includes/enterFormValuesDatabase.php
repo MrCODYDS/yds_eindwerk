408b55acd1691976079ea398839c3d6218756a74
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-06-01 14:21:26 +0200

    email test

--- wp-content/themes/yds/templates/includes/enterFormValuesDatabase.php
+++ wp-content/themes/yds/templates/includes/enterFormValuesDatabase.php
@@ -42,5 +42,13 @@
         "reservation_people" => $people
     ));
     
+    $msg = "First line of text\nSecond line of text";
+
+    // use wordwrap() if lines are longer than 70 characters
+    $msg = wordwrap($msg,70);
+
+    // send email
+    mail("desmetyarne@gmail.com","My subject",$msg);
+
     header('Location: /reservatie');
 ?>
