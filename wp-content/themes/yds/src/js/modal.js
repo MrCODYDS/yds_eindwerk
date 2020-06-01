aabb5c50b7e3531f1c7eb27899dbc232299d98ae
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-06-01 10:26:09 +0200

    delete spacing

--- wp-content/themes/yds/src/js/modal.js
+++ wp-content/themes/yds/src/js/modal.js
@@ -55,8 +55,6 @@ jQuery(document).ready(function ($) {
     var post_url = $(this).attr("action"); //get form action url
     var request_method = $(this).attr("method"); //get form GET/POST method
     var form_data = $(this).serialize(); //Encode form elements for submission
-    console.log(form_data);
-    
     $.ajax({
       url : post_url,
       type: request_method,
--- wp-content/themes/yds/templates/includes/enterFormValuesDatabase.php
+++ wp-content/themes/yds/templates/includes/enterFormValuesDatabase.php
@@ -5,42 +5,42 @@
     global $current_user;
     global $wpdb;
 
-        $choice = $_POST['final-choice'];
-        $people = $_POST['final-people'];
-        $ground = $_POST['final-ground'];
-        $date = $_POST['final-date'];
-        $session = $_POST['sessions'];
-        $userId = $current_user->ID;
+    $choice = $_POST['final-choice'];
+    $people = $_POST['final-people'];
+    $ground = $_POST['final-ground'];
+    $date = $_POST['final-date'];
+    $session = $_POST['sessions'];
+    $userId = $current_user->ID;
 
-        // Check which session is chosen and add length to database
-        switch($session) {
-            case 1:
-                $timeslot = $_POST['radioTimeslots'];
-            break;
-            case 2:
-                $value = $_POST['radioTimeslots'];
-                $timeslot = $value . "," . ($value+1);
-            break;
-            case 3:
-                $value = $_POST['radioTimeslots'];
-                $timeslot = $value . "," . ($value+1) . "," . ($value+2);
-            break;
-            case 4:
-                $value = $_POST['radioTimeslots'];
-                $timeslot = $value . "," . ($value+1) . "," . ($value+2) . "," . ($value+3);
-            break;
-        }
+    // Check which session is chosen and add length to database
+    switch($session) {
+        case 1:
+            $timeslot = $_POST['radioTimeslots'];
+        break;
+        case 2:
+            $value = $_POST['radioTimeslots'];
+            $timeslot = $value . "," . ($value+1);
+        break;
+        case 3:
+            $value = $_POST['radioTimeslots'];
+            $timeslot = $value . "," . ($value+1) . "," . ($value+2);
+        break;
+        case 4:
+            $value = $_POST['radioTimeslots'];
+            $timeslot = $value . "," . ($value+1) . "," . ($value+2) . "," . ($value+3);
+        break;
+    }
 
-        $table = $wpdb->prefix . "user_reservations";
-       
-        $success=$wpdb->insert($table, array(
-            "user_id" => $userId,
-            "reservation_choice" => $choice,
-            "reservation_date" => $date,
-            "reservation_ground" => $ground,
-            "reservation_time" => $timeslot,
-            "reservation_people" => $people
-        ));
-
-        header('Location: /reservatie');
+    $table = $wpdb->prefix . "user_reservations";
+    
+    $success=$wpdb->insert($table, array(
+        "user_id" => $userId,
+        "reservation_choice" => $choice,
+        "reservation_date" => $date,
+        "reservation_ground" => $ground,
+        "reservation_time" => $timeslot,
+        "reservation_people" => $people
+    ));
+    
+    header('Location: /reservatie');
 ?>
