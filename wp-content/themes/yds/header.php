9648fdbf225302006a3a8a91bbb0fac31e3a8600
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-06-02 20:00:42 +0200

    fix that only header__bottom shows as is-fixed and not all header (full header is too big)

--- wp-content/themes/yds/header.php
+++ wp-content/themes/yds/header.php
@@ -43,7 +43,7 @@
             </div>
         </div>
     </div>
-    <div class="c-header__bottom py-2 py-md-4">
+    <div class="c-header__bottom py-2 py-md-3">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-auto d-flex align-items-center">
