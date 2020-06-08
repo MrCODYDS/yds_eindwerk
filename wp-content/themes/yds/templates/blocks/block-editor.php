787a0b3ca3f6234f269c9a9fc96c1e015cc44492
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-06-08 14:26:41 +0200

    added logo for favicon & changed block-editor

--- wp-content/themes/yds/templates/blocks/block-editor.php
+++ wp-content/themes/yds/templates/blocks/block-editor.php
@@ -2,21 +2,26 @@
 $spacer_top = (get_field('spacing_top_editor')) ? 'pt-5 pt-md-4 pt-lg-5' : '';
 $spacer_bottom = (get_field('spacing_bottom_editor')) ? 'pb-5 pb-md-4 pb-lg-5' : '';
 $text_align = strtolower('text-'.get_field('text_align_editor'));
-$boxed = get_field('boxed_editor');
 $title = get_field('title_editor');
 $text = get_field('text_editor');
 ?>
 
 <section class="block b-editor <?= $text_align; ?> <?= $spacer_top; ?> <?= $spacer_bottom; ?>">
     <div class="container">
-        <div class="<?php if($boxed): ?>border border-primary p-4 p-lg-6 <?php endif; ?> <?= $text_align; ?>">
-            <?php if($title): ?>
-                <h3><?= $title; ?></h3>
-            <?php endif; ?>
+        <div class="row">
+            <div class="col">
+                <div class="block-background d-flex flex-wrap py-4 py-md-5 px-3 px-md-4">
+                    <div class="<?= $text_align; ?>">
+                        <?php if($title): ?>
+                            <h3><?= $title; ?></h3>
+                        <?php endif; ?>
 
-            <?php if($text): ?>
-                <p><?= $text; ?></p>
-            <?php endif; ?>
+                        <?php if($text): ?>
+                            <p><?= $text; ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
