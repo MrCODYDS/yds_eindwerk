cc5e965f7909aacff8c42f28f922bbc775605ac4
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-06-09 19:19:47 +0200

    Added message if there is no place or field

--- wp-content/themes/yds/templates/includes/getFreeTimeslots.php
+++ wp-content/themes/yds/templates/includes/getFreeTimeslots.php
@@ -77,18 +77,23 @@ for ($i = 0; $i <= $nowHour; $i++) {
 
 
 // Show all timeslots in modal
-foreach ($hours as $hour) {
-    $hourlength = strlen((string)$hour);
-    
-    echo '<div class="col-4 col-sm-3 mb-3 text-center">';
-    echo '<input type="radio" id="radiotimeslot' . $hour . '" name="radioTimeslots" value="' . $hour .'">';
-    if ($hourlength == 1) {
-        echo '<label for="radiotimeslot' . $hour . '" class="w-100">0'. $hour . ':00</label>';
-    } else {
-        echo '<label for="radiotimeslot' . $hour . '" class="w-100">'. $hour . ':00</label>';
+if (!empty($hours)) {
+    foreach ($hours as $hour) {
+        $hourlength = strlen((string)$hour);
+        
+        echo '<div class="col-4 col-sm-3 mb-3 text-center">';
+        echo '<input type="radio" id="radiotimeslot' . $hour . '" name="radioTimeslots" value="' . $hour .'">';
+        if ($hourlength == 1) {
+            echo '<label for="radiotimeslot' . $hour . '" class="w-100">0'. $hour . ':00</label>';
+        } else {
+            echo '<label for="radiotimeslot' . $hour . '" class="w-100">'. $hour . ':00</label>';
+        }
+        echo '</div>';
     }
-    echo '</div>';
+} else {
+    echo '<div class="col">Momenteel zijn er op de gekozen datum, speelveld en sessielengte geen plaatsen meer vrij. Gelieve een andere dag, terrein of sessielengte te kiezen.</div>';
 }
+
 
 
 ?>
