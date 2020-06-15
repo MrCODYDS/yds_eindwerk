cb3002998b7351235f4d258f09a201adfc5a8036
Yarne De Smet <32762817+MrCODYDS@users.noreply.github.com>
2020-06-15 14:32:34 +0200

    deleted double code in getamountofpeople.php

--- wp-content/themes/yds/templates/includes/getAmountOfPeople.php
+++ wp-content/themes/yds/templates/includes/getAmountOfPeople.php
@@ -7,6 +7,7 @@ $choiceValue = $_GET['choice'];
 
 switch ($choiceValue) {
     case "Individuele spelers":
+    case "Privé training":
         for ($i = 1; $i <= 4; $i++) {
             echo '<div class="col-auto mb-3">';
             echo '<input type="radio" id="radioPeople' . $i . '" name="radioPeople" value="' . $i . '">';
@@ -15,28 +16,13 @@ switch ($choiceValue) {
         }
         break;
     case "Clubs":
+        case "Bedrijven":
         echo '<select id="amountPeopleSelect" name="amountPeopleSelect">';
         echo '<option value="" selected="selected" disabled hidden>Geen aantal geselecteerd</option>';
         for ($i = 1; $i <= 50; $i++) {
             echo '<option value="' . $i .'">' .$i . '</option>';
         }
         echo '</select>';
-        break;
-    case "Bedrijven":
-        echo '<select id="amountPeopleSelect" name="amountPeopleSelect">';
-        echo '<option value="" selected="selected" disabled hidden>Geen aantal geselecteerd</option>';
-        for ($i = 1; $i <= 50; $i++) {
-            echo '<option value="' . $i .'">' .$i . '</option>';
-        }
-        echo '</select>';
-        break;
-    case "Privé training":
-        for ($i = 1; $i <= 4; $i++) {
-            echo '<div class="col-auto mb-3">';
-            echo '<input type="radio" id="radioPeople' . $i . '" name="radioPeople" value="' . $i . '">';
-            echo '<label for="radioPeople' . $i . '">' .$i .'</label>';
-            echo '</div>';
-        }
         break;
 }
 ?>
